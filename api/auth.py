d569a9074fa31494a59a800451bec561b6e60273
evaristofm <evaristofm@gmail.com>
2021-03-02 12:29:38 -0300

    Initial commit

---
+++ api/auth.py
@@ -0,0 +1,27 @@
+import jwt
+from passlib.hash import bcrypt
+from fastapi import Depends, HTTPException, status
+from .models import User, User_Pydantic
+from fastapi.security import OAuth2PasswordBearer
+
+JWT_SECRET = 'myjwtsecret'
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+
+
+async def authenticate_user(username: str, password: str):
+    user = await User.get(username=username)
+    if not user:
+        return False
+    if not user.verify_password(password):
+        return False
+    return user
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
+        user = await User.get(id=payload.get('id'))
+    except:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+    detail='Invalid username or password')
+
+    return await User_Pydantic.from_tortoise_orm(user)
---
+++ api/main.py
@@ -0,0 +1,49 @@
+import jwt
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from tortoise.contrib.fastapi import register_tortoise
+
+from .auth import JWT_SECRET, authenticate_user, get_current_user
+from .models import User, User_Pydantic, UserIn_Pydantic
+
+app = FastAPI()
+
+
+
+@app.get('/')
+async def main():
+    return {"Hello": "World"}
+
+@app.post('/token')
+async def generate_token(data: OAuth2PasswordRequestForm = Depends()):
+    user = await authenticate_user(data.username, data.password)
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Invalid username or password')
+    
+    user_obj = await User_Pydantic.from_tortoise_orm(user)
+    token = jwt.encode(user_obj.dict(), JWT_SECRET)
+
+    return {'access_token': token, 'token_type': 'bearer'}
+
+@app.post('/users', response_model=User_Pydantic)
+async def create_user(user: UserIn_Pydantic):
+    user_obj = User(username=user.username, password_hash=bcrypt.hash(user.password_hash))
+    await user_obj.save()
+    return await User_Pydantic.from_tortoise_orm(user_obj)
+
+@app.get('/users/me', response_model=User_Pydantic)
+async def get_user(user: User_Pydantic = Depends(get_current_user)):
+    return user
+
+
+register_tortoise(
+    app,
+    db_url='sqlite://db.sqlite3',
+    modules={'models': ['api.models']},
+    generate_schemas=True,
+    add_exception_handlers=True
+)
+
+
